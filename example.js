2cf168570fb8231322c1ee2a677d25526aa6cfb5
annaguckian <alg7@st-andrews.ac.uk>
2016-10-31 17:17:28 +0000

    JavaScript Attempt1

---
+++ example.js
@@ -0,0 +1,16 @@
+
+function calc(x,y) {
+	var result;
+	if(x==y){
+		result = "even";
+	}
+	else{
+		result = "uneven";
+		
+	}
+	return result;
+
+}
+
+console.log(calc(3,3));
+console.log(calc(20,6));
