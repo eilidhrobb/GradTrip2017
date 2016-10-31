4cbaa31dd193718aadf209ba76880ead0ba769bb
annaguckian <alg7@st-andrews.ac.uk>
2016-10-31 18:03:03 +0000

    Added location list

--- example.js
+++ example.js
@@ -1,2 +1,16 @@
+$(document).ready(function(){
+	
+	$("#addbutton").on("click", function(){
+		var value = $("#input").val();
+		if(value !== ""){
+			$("#list").append("<li>" + value + "</li>");
+			$("#input").val("");
+		}
+	});
+});
+
+
+
+
 
 
