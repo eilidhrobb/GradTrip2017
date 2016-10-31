d9ade3d4c247d0847ea1e360d58cbae683272a43
annaguckian <alg7@st-andrews.ac.uk>
2016-10-31 17:43:18 +0000

    jQuery

--- example.js
+++ example.js
@@ -1,16 +1,2 @@
 
-function calc(x,y) {
-	var result;
-	if(x==y){
-		result = "even";
-	}
-	else{
-		result = "uneven";
-		
-	}
-	return result;
 
-}
-
-console.log(calc(3,3));
-console.log(calc(20,6));
